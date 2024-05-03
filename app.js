51fc59d4c13b2c4073bf43ccf6b867d33c6208d8
payaltalreja08 <payaltalreja0808@gmail.com>
2024-05-03 11:11:41 +0530

    HTML,CSS DONE

---
+++ app.js
@@ -0,0 +1,17 @@
+const choices=document.querySelectorAll(".choice");
+let UserScore=0;
+let CompScore=0;
+const msg=document.getElementById("msg");
+const msgContainer=document.getElementsByClassName("msg-box");
+const User_Score=document.getElementById("user-score");
+const Comp_Score=document.getElementById("comp-score");
+
+
+//User Choice
+choices.forEach((choice)=>{
+choice.addEventListener("click",()=>{
+  const UserChoice=choice.getAttribute("id");
+});
+});
+
+//compchoice
